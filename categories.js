dbd65048f95971cfac029171045fd5f9ddeb0f9b
7mo <afkfdchyr356@gmail.com>
2026-04-14 21:51:32 +0200

    Update Categories

---
+++ categories.js
@@ -0,0 +1,52 @@
+const MENU_CATEGORIES = [
+  {
+    "label": "كل الأصناف",
+    "value": "all",
+    "icon": ""
+  },
+  {
+    "label": "عروض حصرية",
+    "value": "offer",
+    "icon": "🔥"
+  },
+  {
+    "label": "الأكثر مبيعاً",
+    "value": "hot",
+    "icon": "🔥"
+  },
+  {
+    "label": "جديد",
+    "value": "new",
+    "icon": "🆕"
+  },
+  {
+    "label": "بيتزا",
+    "value": "بيتزا",
+    "icon": "🍕"
+  },
+  {
+    "label": "وافل",
+    "value": "وافل",
+    "icon": "🧇"
+  },
+  {
+    "label": "برجر",
+    "value": "برجر",
+    "icon": "🍔"
+  },
+  {
+    "label": "كريب",
+    "value": "كريب",
+    "icon": "🌮"
+  },
+  {
+    "label": "بطاطس",
+    "value": "مقبلات",
+    "icon": "🍟"
+  },
+  {
+    "label": "مشروبات",
+    "value": "مشروبات",
+    "icon": "🥤"
+  }
+];
